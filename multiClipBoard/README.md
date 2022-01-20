c0e05e047cf6f7f8c49532bc95cd20443c1e5ad3
Fernando Barrios <barrioslui02@gmail.com>
2022-01-20 16:18:05 -0600

    Add working code

--- multiClipBoard/README.md
+++ multiClipBoard/README.md
@@ -4,7 +4,12 @@
 
 This project uses the [pyperclip module](https://pypi.org/project/pyperclip/)
 
-Usage:
+### Installation:
 * Install the pyperclip module using the _pip_ package manager.
 * Execute the .py file using Python3.6+
-* The program uses command-line arguments (save, list)
+* The program uses command-line arguments (save, load,list)
+
+### Usage:
+* Provide a command-line argument **(save, load, list)**
+* For *save* and *load* the program will require a key
+* The data is stored in a auto-generated **.json** file
--- multiClipBoard/multiclipboard.py
+++ multiClipBoard/multiclipboard.py
@@ -1,10 +1,49 @@
-#! usr/bin/python3
+#!/usr/bin/env python3
 
+
+from ast import Try
 import sys
 import json
+
+from simplejson import load
 import pyperclip as pc
 
+SAVED_DATA = 'clipboard.json'
 
-data = pc.paste()
+def save_items(filepath, data):
+    with open(filepath, "w") as f:
+        json.dump(data, f)
 
-print(sys.argv)
+def load_data(filepath):
+    try:
+        with open(filepath, "r") as f:
+            data = json.load(f)
+            return data
+    except:
+        return {}
+
+if len(sys.argv) == 2:
+
+    data = load_data(SAVED_DATA)
+
+    if sys.argv[1] == 'save':
+        key = input('Enter a key: ')
+        data[key] = pc.paste()
+        save_items(SAVED_DATA, data)
+        print("Data has been Saved")
+    elif sys.argv[1] == 'load':
+        key = input('Enter a key: ')
+        if key in data: 
+            pc.copy(data[key])
+            print('Data copied to clipboard.')
+        else:
+            print("Key doesn't exist")
+    elif sys.argv[1] == 'list':
+        print(data)
+    else:
+        print('Bad input')
+else:
+    print(f'Program uses exactly one argument')
+
+
+
